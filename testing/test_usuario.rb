6376962134650c75bab64d41eaa6a48f3eabfa26
mati <mati331@correo.ugr.es>
2019-10-28 14:49:37 +0100

    quito json closes #23

--- testing/test_usuario.rb
+++ testing/test_usuario.rb
@@ -1,19 +1,19 @@
 
 require_relative  'src/Usuarios/usuario'
 require  'test/unit'
-require  "json"
+#require  "json"
 
 class TestMyUsuario <  Test::Unit::TestCase
 
-	def setup 
-		file = File.read('src/Usuarios/ejemploUsuario.json')
-		data_hash = JSON.parse(file)
-		
-		@usuario = usuario.new(data_hash['Identificador'], data_hash['Nombre'], data_hash['Correo'], data_hash['Direccion envio'], data_hash['Metodo pago'])
-	end
+#	def setup 
+#		file = File.read('src/Usuarios/ejemploUsuario.json')
+#		data_hash = JSON.parse(file)
+#		
+#		@usuario = usuario.new(data_hash['Identificador'], data_hash['Nombre'], data_hash['Correo'], data_hash['Direccion envio'], data_hash['Metodo pago'])
+#	end
 	
 	def test_usuario()
-		#@usuario = usuario.new("00101","20","2 kg", "20,20,50","Botella de ron")
+		@usuario = usuario.new("00001","Matilde","mati331@correo.ugr.es", "Daniel Saucedo","Al contado")
 		assert @usuario.getIdentificador == "00001", " Identificador erroneo"
 		assert @usuario.getNombre == "Matilde", "El nombre no es correcto"
 		assert @usuario.getCorreo == "mati331@correo.ugr.es", " Correo erroneo "
