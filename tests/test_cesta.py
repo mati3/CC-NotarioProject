e245c5b75dc2c20c1f216e9de8214d6f4759561b
mati <mati331@correo.ugr.es>
2019-11-11 13:52:54 +0100

    add json to cesta

--- tests/test_cesta.py
+++ tests/test_cesta.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import unittest
+import json
 import sys, os.path
 
 dir_path = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))+ '/src/Cesta/')
@@ -10,7 +15,10 @@ from cesta import Cesta
 class TestCesta(unittest.TestCase): 
 
     def test_cesta(self):
-        c = Cesta("mati331@correo.ugr.es", "00101", "En proceso", "Completado")
+        with open('ejemploCesta.json','r') as myfile:
+            obj = myfile.read()
+            c = Cesta(obj['Correo'], obj['Articulo'], obj['Estado de la compra'], obj['Estado del pago'])
+        #c = Cesta("mati331@correo.ugr.es", "00101", "En proceso", "Completado")
 
         self.assertEqual(c.getCorreo(),"mati331@correo.ugr.es", "correo incorrecto")
         self.assertEqual(c.getArticulo(), "00101", "articulo incorrecto")
