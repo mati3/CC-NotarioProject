3bbf64e6527d798cf9b7c1fc0f8506e5162a02e8
mati <mati331@correo.ugr.es>
2019-12-03 18:58:26 +0100

    add codecov ruby

--- Catalogo/tests/test_producto.rb
+++ Catalogo/tests/test_producto.rb
@@ -2,6 +2,11 @@
 require_relative  '../src/producto'
 require  'test/unit'
 require 'rack/test'
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 class TestMyProducto <  Test::Unit::TestCase
 	include Rack::Test::Methods
