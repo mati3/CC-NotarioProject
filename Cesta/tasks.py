db4015ad8af2f520ba1f53c5dfc197413c88971d
mati <mati331@correo.ugr.es>
2019-12-25 20:02:27 +0100

    automatize task gunicorn close #29 close #30

--- Cesta/tasks.py
+++ Cesta/tasks.py
@@ -3,5 +3,9 @@ from invoke import task
 
 @task
 def test(c):
-    c.run("pytest -q tests/test_cesta.py")
+    c.run("pytest -q tests/test*.py")
+
+@task
+def start(c):
+    c.run("gunicorn -w 5 appCesta:app")
 
